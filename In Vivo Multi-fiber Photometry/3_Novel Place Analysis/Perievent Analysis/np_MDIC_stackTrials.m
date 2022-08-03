21c9bbaf03af29ea81eea77617ef4143181950d8
Hechen Bao <61467224+HechenBao@users.noreply.github.com>
2022-08-03 10:47:41 -0400

    Update np_MDIC_stackTrials.m

--- In Vivo Multi-fiber Photometry/3_Novel Place Analysis/Perievent Analysis/np_MDIC_stackTrials.m
+++ In Vivo Multi-fiber Photometry/3_Novel Place Analysis/Perievent Analysis/np_MDIC_stackTrials.m
@@ -53,60 +53,6 @@ end
     after = 30;
     t=[-before:after]./10;
  
-% figure;
-%     x0=100;
-%     y0=100;
-%     width=1200;
-%     height=400;
-%     set(gcf,'position',[x0,y0,width,height])    
-%         
-% %Old place
-% subplot(2,4,1)
-% imagesc(sortrows(rasterOld_IC,31,'descend'))
-% %xlim([0 before+after])
-% title('IC Old Place')
-% caxis([-1 2]);
-% colorbar;
-% subplot(2,4,2)
-% plot(t,mean(rasterOld_IC))
-% xlim([t(1) t(length(t))])
-% hold on
-% 
-% subplot(2,4,5)
-% imagesc(sortrows(rasterOld_MD,31,'descend'))
-% %xlim([0 before+after])
-% title('MD Old Place')
-% caxis([-1 2]);
-% colorbar;
-% subplot(2,4,6)
-% plot(t,mean(rasterOld_MD))
-% xlim([t(1) t(length(t))])
-% hold on
-% 
-% %Novel place
-% subplot(2,4,3)
-% imagesc(sortrows(rasterNovel_IC,31,'descend'))
-% %xlim([0 before+after])
-% title('IC Novel Place')
-% caxis([-1 2]);
-% colorbar;
-% subplot(2,4,4)
-% plot(t,mean(rasterNovel_IC))
-% xlim([t(1) t(length(t))])
-% hold on
-% 
-% subplot(2,4,7)
-% imagesc(sortrows(rasterNovel_MD,31,'descend'))
-% %xlim([0 before+after])
-% title('MD Novel Place')
-% caxis([-1 2]);
-% colorbar;
-% subplot(2,4,8)
-% plot(t,mean(rasterNovel_MD))
-% xlim([t(1) t(length(t))])
-% hold on
-
-
 figure;
     x0=100;
     y0=100;
